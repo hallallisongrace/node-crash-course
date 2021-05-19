c64e0ce0272dbfa622f8504f6024f08c5921601c
Allison Hall <allison@Allisons-MacBook-Pro.local>
2021-05-18 20:55:18 -0400

    node

---
+++ people.js
@@ -0,0 +1,8 @@
+const people = ['Allison', 'Christian', 'Leslie'];
+const ages = ['24', '26', '2'];
+
+console.log(people);
+
+module.exports = {
+    people, ages
+};
---
+++ test.js
@@ -0,0 +1,10 @@
+//const name = 'Christian';
+
+//console.log(name);
+
+const greet = (name) => {
+    console.log(`hello', ${name}`);
+}
+
+greet('Allison');
+greet('Christian');
