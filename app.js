9401d73313cc74d7a56ad277b6584604185054fd
Allison Hall <allison@Allisons-MacBook-Pro.local>
2021-05-20 17:30:09 -0400

    update

--- app.js
+++ app.js
@@ -1,31 +1,65 @@
 const express = require('express');
+const morgan = require('morgan');
 
-//express app
+// express app
 const app = express();
 
-//listen for requests
+// listen for requests
 app.listen(3000);
 
+
+//middleware & static files
+app.use(express.static('public'));
+app.use(morgan('dev'));
+
+
+
+
+// register view engine 
+app.set('view engine', 'ejs');
+
+
+const blogs = [
+  {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+  {title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+  {title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+];
+
+
+
+
 app.get('/', (req, res) => {
+    res.render('index', {title: 'Home', blogs});
+  });
 
-    //res.send('<p>home page</p>');
-    res.sendFile('./views/index.html', { root: __dirname });
+  app.get('/about', (req, res) => {
+    res.render('about', {title: 'About'});
+  });
+
+  app.get('/blogs/create', (req, res) => {
+    res.render('create', {title: 'Create a new blog'});
+  });
+
+  // 404 page
+  app.use((req, res) => {
+    res.render('404', {title: '404'}).render('404');
+  });
+
+app.get('/', (req, res) => {
+    // res.send('<p>home page</p>');
+    res.sendFile('./views/index.html', {root: __dirname});
 });
-
 
 app.get('/about', (req, res) => {
-
-    //res.send('<p>about page</p>');
-    res.sendFile('./views/about.html', { root: __dirname });
+    // res.send('<p>about page</p>');\
+    res.sendFile('./views/about.html', {root: __dirname});
 });
 
-//redirects
-app.get('.about-us', (req, res) => {
-    res.redirect('/about');  
-})
-
-
-//404
+// redirects
+// app.get('/about-us', (req, res) => {
+//     res.redirect('/about');
+// })
+// 404 page
 app.use((req, res) => {
-    res.status(404).sendFile('./views/404.html', {root: __dirname});
-});
+    res.status(404).sendFile('./views/404.html', { root: __dirname });
+})
