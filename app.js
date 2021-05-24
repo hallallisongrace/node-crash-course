a2ecba2f8b7a1aebbe8705bcf8ae2647132a705a
Allison Hall <allison@Allisons-MacBook-Pro.local>
2021-05-23 22:54:39 -0400

    update

--- app.js
+++ app.js
@@ -1,65 +1,43 @@
 const express = require('express');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
+const blogRoutes = require('./routes/blogRoutes');
 
 // express app
 const app = express();
 
-// listen for requests
-app.listen(3000);
+// connect to mongodb & listen for requests
+const dbURI = "mongodb+srv://netninja:test1234@net-ninja-tuts-del96.mongodb.net/node-tuts";
 
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(result => app.listen(3000))
+  .catch(err => console.log(err));
 
-//middleware & static files
-app.use(express.static('public'));
-app.use(morgan('dev'));
-
-
-
-
-// register view engine 
+// register view engine
 app.set('view engine', 'ejs');
 
+// middleware & static files
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan('dev'));
+app.use((req, res, next) => {
+  res.locals.path = req.path;
+  next();
+});
 
-const blogs = [
-  {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-  {title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-  {title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-];
-
-
-
-
+// routes
 app.get('/', (req, res) => {
-    res.render('index', {title: 'Home', blogs});
-  });
-
-  app.get('/about', (req, res) => {
-    res.render('about', {title: 'About'});
-  });
-
-  app.get('/blogs/create', (req, res) => {
-    res.render('create', {title: 'Create a new blog'});
-  });
-
-  // 404 page
-  app.use((req, res) => {
-    res.render('404', {title: '404'}).render('404');
-  });
-
-app.get('/', (req, res) => {
-    // res.send('<p>home page</p>');
-    res.sendFile('./views/index.html', {root: __dirname});
+  res.redirect('/blogs');
 });
 
 app.get('/about', (req, res) => {
-    // res.send('<p>about page</p>');\
-    res.sendFile('./views/about.html', {root: __dirname});
+  res.render('about', { title: 'About' });
 });
 
-// redirects
-// app.get('/about-us', (req, res) => {
-//     res.redirect('/about');
-// })
+// blog routes
+app.use('/blogs', blogRoutes);
+
 // 404 page
 app.use((req, res) => {
-    res.status(404).sendFile('./views/404.html', { root: __dirname });
-})
+  res.status(404).render('404', { title: '404' });
+});
